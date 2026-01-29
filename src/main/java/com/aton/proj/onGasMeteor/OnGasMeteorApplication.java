ae0d147a3d86a7c3233392f588d24d60556ecb02
Dario.Cescon <Dario.Cescon@aton.com>
2026-01-29 13:04:53 +0100

    Init

---
+++ src/main/java/com/aton/proj/onGasMeteor/OnGasMeteorApplication.java
@@ -0,0 +1,13 @@
+package com.aton.proj.onGasMeteor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnGasMeteorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnGasMeteorApplication.class, args);
+	}
+
+}
